c758ac64dcec55b70cf41342cbc567649718e611
Jan-Erik Rediger <janerik@fnordig.de>
2025-06-02 15:21:02 +0200

    Strip down example

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use std::sync::{Arc, RwLock};
-use std::time::Duration;
-
-use async_std::future::{pending, timeout};
+use std::sync::Arc;
 
 #[derive(Debug, thiserror::Error, uniffi::Error)]
 pub enum LaunchError {
@@ -25,110 +22,16 @@ pub struct Part {
     pub weight: i64,
 }
 
-
-/// Async function that says something after a certain time.
-#[uniffi::export]
-async fn launch_after(rocket: Arc<Rocket>, ms: u64) -> Result<bool> {
-    let never = pending::<()>();
-    timeout(Duration::from_millis(ms), never).await.unwrap_err();
-    rocket.launch()
-}
-
 /// A rocket we can launch into orbit.
 #[derive(uniffi::Object)]
-pub struct Rocket(RwLock<Inner>);
+pub struct Rocket;
 
 #[uniffi::export]
 impl Rocket {
     #[uniffi::constructor]
-    pub fn new(name: String) -> Arc<Self> {
-        Arc::new(Self(RwLock::new(Inner::new(name))))
-    }
-
-    fn add(&self, part: Part) {
-        let mut rocket = self.0.write().unwrap();
-        rocket.add(part)
-    }
-
-    fn lock_steering(&self, direction: Direction) {
-        let mut rocket = self.0.write().unwrap();
-        rocket.lock_steering(direction);
-    }
-
-    fn launch(&self) -> Result<bool> {
-        let rocket = self.0.read().unwrap();
-        rocket.launch()
-    }
-
-    fn show(&self) -> String {
-        let rocket = self.0.read().unwrap();
-        rocket.show()
+    pub fn new(_name: String) -> Arc<Self> {
+        Arc::new(Self)
     }
 }
-
-struct Inner {
-    name: String,
-    parts: Vec<Part>,
-    total_cost: i64,
-    total_weight: i64,
-    steering: Option<Direction>,
-}
-
-impl Inner {
-    /// Construct a new named rocket.
-    fn new(name: String) -> Self {
-        Inner {
-            name,
-            parts: vec![],
-            total_cost: 0,
-            total_weight: 0,
-            steering: None,
-        }
-    }
-
-    /// Add a new part to the rocket.
-    fn add(&mut self, part: Part) {
-        self.total_cost += part.cost;
-        self.total_weight += part.weight;
-        self.parts.push(part);
-    }
-
-    /// Lock the steering into a single direction.
-    fn lock_steering(&mut self, dir: Direction) {
-        self.steering = Some(dir);
-    }
-
-    /// Try to launch the rocket into orbit.
-    ///
-    /// This will throw a `LaunchError` if the rocket is not pointing up.
-    /// Returns `true` if the launch succeeded
-    /// or `false` if the rocket was too heavy or too expensive.
-    fn launch(&self) -> Result<bool> {
-        match self.steering {
-            None | Some(Direction::Down) => return Err(LaunchError::RocketLaunch),
-            _ => {}
-        }
-
-        if self.total_cost < 2000 && self.total_weight < 5000 {
-            Ok(true)
-        } else {
-            Ok(false)
-        }
-    }
-
-    /// Show the rocket's configuration.
-    fn show(&self) -> String {
-        format!(
-            "Rocket({}) {{ cost: {}, weight: {}, #parts: {}, steering: {:?} }}",
-            self.name,
-            self.total_cost,
-            self.total_weight,
-            self.parts.len(),
-            self.steering
-        )
-    }
-}
-
-type Result<T, E = LaunchError> = std::result::Result<T, E>;
 
 uniffi::setup_scaffolding!();
